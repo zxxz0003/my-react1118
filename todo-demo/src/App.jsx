3571f6421899270a9eb936322074b5c3eb4cbf4f
zxxz0003 <g9512323@gmail.com>
2024-12-02 10:44:30 +0800

    2

--- todo-demo/src/App.jsx
+++ todo-demo/src/App.jsx
@@ -1,12 +1,34 @@
-import "./App.css"
-import TodoWrapper from "./components/TodoWrapper";
+import { useEffect } from "react"
+import axios from 'axios';
 function App(){
-  console.log("123");
+
+/ useEffect(()=>{
+(async()=>{
+  const data =await axios.get('./todo-demo/F-C0032-001.json');
+  const{location}=data.data.cwaopendata.dataset;
+  console.log(locaton);
+})
+  },[]) 
 
   return (
     <>
-   <TodoWrapper/>
-   
+  <h2>36小時天氣</h2>
+  <h3>台北市</h3>
+  <h3>2日</h3> 
+  <h5>上午6:00</h5>
+          ~
+  <h5>下午6:00</h5>
+  
+  <div class="card-img-top" style={{
+                                   
+                                   height:'200px', 
+                                   backgroundImage:`url('todo-demo/weatherIcon/多雲.svg')`,
+                                  backgroundSize:"cover",
+                                  backgroundPosition:"center center",
+                               }}>123</div>
+  
+
+    
     </>
   )
 }
